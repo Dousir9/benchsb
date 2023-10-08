dba30517a52a8323284cccd72cba0fbc2f9690f3
BohuTANG <overred.shuttler@gmail.com>
2023-10-08 10:56:42 +0800

    upgrade to bendsql latest version

--- bend/bend.py
+++ bend/bend.py
@@ -56,13 +56,13 @@ def main():
 
     with open('query_results.txt', 'w') as result_file:
         for index, query in enumerate(queries):
-            print(f"Executing SQL-{index + 1}: {query}")
 
             # Suspend the warehouse before executing the query, unless --nosuspend is specified
             if not args.nosuspend:
                 suspend_warehouse(warehouse)
 
             try:
+                print(f"Executing SQL: {query}")
                 output = execute_sql(query)
                 time_elapsed = extract_time(output)
                 print(f"Time Elapsed: {time_elapsed}s\n")
