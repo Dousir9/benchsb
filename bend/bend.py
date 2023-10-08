f0e6a35a5dde412bfdb96d158f54feb0656b86c4
BohuTANG <overred.shuttler@gmail.com>
2023-10-08 10:51:12 +0800

    upgrade to bendsql latest version

--- bend/bend.py
+++ bend/bend.py
@@ -1,8 +1,9 @@
 import subprocess
 import re
 import sys
+import os
 from pathlib import Path
-
+import argparse
 
 def execute_sql(query):
     command = ['bendsql', '--query=' + query, '--time']
@@ -12,15 +13,37 @@ def execute_sql(query):
     except subprocess.CalledProcessError as e:
         sys.exit(f"bendsql command failed: {e.stderr}")
 
-
 def extract_time(output):
     match = re.search(r'([0-9.]+)$', output)
     if not match:
         raise ValueError("Could not extract time from output.")
     return match.group(1)
 
+def get_warehouse_from_env():
+    dsn = os.environ.get('BENDSQL_DSN', '')
+    match = re.search(r'--([\w-]+)\.gw', dsn)
+    if not match:
+        raise ValueError("Could not extract warehouse name from BENDSQL_DSN.")
+    return match.group(1)
+
+
+def suspend_warehouse(warehouse):
+    # Construct the SQL command to suspend the warehouse
+    suspend_sql = f"ALTER WAREHOUSE '{warehouse}' SUSPEND;"
+    try:
+        execute_sql(suspend_sql)
+        print(f"Warehouse {warehouse} suspended.")
+    except Exception as e:
+        sys.exit(f"Failed to suspend warehouse {warehouse}: {e}")
 
 def main():
+    parser = argparse.ArgumentParser(description='Execute SQL queries and optionally suspend the warehouse.')
+    parser.add_argument('--nosuspend', action='store_true',
+                        help='Do not suspend the warehouse before executing the query')
+    args = parser.parse_args()
+
+    warehouse = get_warehouse_from_env()
+
     sql_file_path = Path('./queries.sql')
     if not sql_file_path.exists():
         sys.exit("SQL file does not exist.")
@@ -34,6 +57,10 @@ def main():
     with open('query_results.txt', 'w') as result_file:
         for index, query in enumerate(queries):
             print(f"Executing SQL-{index + 1}: {query}")
+
+            # Suspend the warehouse before executing the query, unless --nosuspend is specified
+            if not args.nosuspend:
+                suspend_warehouse(warehouse)
 
             try:
                 output = execute_sql(query)
@@ -49,7 +76,6 @@ def main():
     print("Overall Execution Results:")
     for result in results:
         print(result)
-
 
 if __name__ == "__main__":
     main()
