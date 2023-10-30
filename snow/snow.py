749844408e01da523c1c3f95ef195a1aa036d92b
BohuTANG <overred.shuttler@gmail.com>
2023-10-30 22:53:40 +0800

    Add bend dsn private link format

--- snow/snow.py
+++ snow/snow.py
@@ -8,11 +8,15 @@ import time
 def execute_sql(query, warehouse):
     """Execute an SQL query using snowsql."""
     command = [
-        'snowsql',
-        '--warehouse', warehouse,
-        '--schemaname', 'PUBLIC',
-        '--dbname', 'tpch_sf100',
-        '-q', query
+        "snowsql",
+        "--warehouse",
+        warehouse,
+        "--schemaname",
+        "PUBLIC",
+        "--dbname",
+        "tpch_sf100",
+        "-q",
+        query,
     ]
 
     try:
@@ -24,7 +28,7 @@ def execute_sql(query, warehouse):
 
 def extract_time(output):
     """Extract execution time from the snowsql output."""
-    match = re.search(r'Time Elapsed:\s*([0-9.]+)s', output)
+    match = re.search(r"Time Elapsed:\s*([0-9.]+)s", output)
     return match.group(1) if match else None
 
 
@@ -41,27 +45,38 @@ def restart_warehouse(warehouse):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Execute SQL queries and optionally restart the warehouse.')
-    parser.add_argument('--nosuspend', action='store_false', default=True,
-                        dest='suspend', help='Do not run the alter statement to suspend the warehouse')
-    parser.add_argument('--warehouse', default='COMPUTE_WH', help='Specify the warehouse to use. Defaults to COMPUTE_WH')
+    parser = argparse.ArgumentParser(
+        description="Execute SQL queries and optionally restart the warehouse."
+    )
+    parser.add_argument(
+        "--nosuspend",
+        action="store_false",
+        default=True,
+        dest="suspend",
+        help="Do not run the alter statement to suspend the warehouse",
+    )
+    parser.add_argument(
+        "--warehouse",
+        default="COMPUTE_WH",
+        help="Specify the warehouse to use. Defaults to COMPUTE_WH",
+    )
     args = parser.parse_args()
 
-    sql_file_path = Path('./queries.sql')
+    sql_file_path = Path("./queries.sql")
 
     if not sql_file_path.exists():
         print("SQL file does not exist.")
         return
 
-    with open(sql_file_path, 'r') as f:
+    with open(sql_file_path, "r") as f:
         content = f.read()
 
-    queries = [query.strip() for query in content.split(';') if query.strip()]
+    queries = [query.strip() for query in content.split(";") if query.strip()]
     results = []
 
     # Disable caching of results
     execute_sql("ALTER ACCOUNT SET USE_CACHED_RESULT=FALSE;", args.warehouse)
-    with open('query_results.txt', 'w') as result_file:
+    with open("query_results.txt", "w") as result_file:
         for index, query in enumerate(queries):
             print(f"Executing SQL-{index + 1}: {query}")
 
@@ -74,11 +89,15 @@ def main():
             time_elapsed = extract_time(output)
             if time_elapsed:
                 print(f"Time Elapsed: {time_elapsed}s\n")
-                result_file.write(f"SQL-{index + 1}: {query}\nTime Elapsed: {time_elapsed}s\n\n")
+                result_file.write(
+                    f"SQL-{index + 1}: {query}\nTime Elapsed: {time_elapsed}s\n\n"
+                )
                 results.append(time_elapsed)
             else:
                 print("Could not extract time from output.\n")
-                result_file.write(f"SQL-{index + 1}: {query}\nTime Elapsed: Unknown\n\n")
+                result_file.write(
+                    f"SQL-{index + 1}: {query}\nTime Elapsed: Unknown\n\n"
+                )
                 results.append(f"SQL-{index + 1}: Unknown")
 
     print("Overall Execution Results:")
